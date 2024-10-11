11f6d14dc4ada3cf9bbd6b5c3ca99e22e3ff442d
Randy Wirjadiredja <randy.wirjadiredja@binus.edu>
2024-10-11 15:18:37 +0700

    initialize part 1 code

---
+++ MyUniversity/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyUniversity.Data;
+
+namespace MyUniversity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyUniversity/Models/Major.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyUniversity.Models
+{
+    public class Major
+    {
+        [Key]
+        public int MajorID { get; set; }
+
+        [MaxLength(50)]
+        public string MajorName { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
