30a4c188a351b6ac5632c67d199841e730a92034
Jackson Ang <jackson.ang@binus.edu>
2024-10-21 10:34:59 +0700

    Training part 2 - add crud api in controller

---
+++ MyUniversity/Models/Request/UpdateStudentRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace MyUniversity.Models.Request
+{
+    public class UpdateStudentRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int MajorID { get; set; }
+    }
+}
